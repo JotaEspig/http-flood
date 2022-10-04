f1fddd54b71afdc9781a475af5a7f2f5bd123811
João Vitor Espig <jotinha1300@gmail.com>
2022-10-04 19:04:06 -0300

    added get_banner

--- src/main.c
+++ src/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+// Gets the banner of the tool
+char *get_banner() {
+    return  "= = = = = = = = = = = = = = = = = = = =\n" \
+            "              HTTP Flood               \n" \
+            "= = = = = = = = = = = = = = = = = = = =\n" ;
+}
+
 int main() {
-    printf("Começo do projeto\n");
+    char *banner = get_banner();
+    printf("Começo do projeto\n\n%s\n", banner);
+    return 0;
 }
