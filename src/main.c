b917ee3d60d2f7e488744a12d42e0f4fc3b0581f
João Vitor Espig <jotinha1300@gmail.com>
2022-10-04 22:50:33 -0300

    started to handle cli args at main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Gets the banner of the tool
-char *get_banner() {
-    return  "= = = = = = = = = = = = = = = = = = = =\n" \
-            "              HTTP Flood               \n" \
-            "= = = = = = = = = = = = = = = = = = = =\n" ;
+// Prints the banner of the tool
+void print_banner(void) {
+    printf( "= = = = = = = = = = = = = = = = = = = =\n"
+            "              HTTP Flood               \n"
+            "= = = = = = = = = = = = = = = = = = = =\n");
 }
 
-int main() {
-    char *banner = get_banner();
-    printf("Começo do projeto\n\n%s\n", banner);
+// Prints the help string
+void print_help(void) {
+    printf("Usage: http-flood <host> [port] [number of attacks]\n"); // TODO improve help string
+}
+
+int main(int argc, char **argv) {
+    int port, num_attacks;
+    port = 80;
+    num_attacks = 100000000;
+    if (argc < 2) {
+        print_help();
+        exit(1);
+    }
+    printf("host: %s\n", argv[1]);
+    print_banner();
     return 0;
 }
