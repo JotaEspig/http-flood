857588fa189e2baa632cf336d3a3b4bc74fe7598
João Vitor Espig <jotinha1300@gmail.com>
2022-10-07 20:13:03 -0300

    added file for colors

---
+++ src/color.h
@@ -0,0 +1,11 @@
+#ifndef COLOR
+#define COLOR
+
+#define LIGHT_RED "\e[1;31m"
+#define LIGHT_BLUE "\e[1;34m"
+#define CYAN "\e[0;36m"
+#define LIGHT_GREEN "\e[1;32m"
+#define YELLOW "\e[0;33m"
+#define NO_COLOR "\e[0m"
+
+#endif // COLOR
